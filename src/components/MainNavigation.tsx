afa29f83e1c95ece0661c8428b7c98810edb88e2
Hakan Urtimur <dythakanurtimur@gmail.com>
2023-06-07 23:03:31 +0300

    fix something

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const MainNavigation: React.FC = () => {
     return (
         <header className='flex flex-row items-center justify-between bg-blue-800 text-blue-50  px-1.5 py-5 gap-4 rounded'>
-            <div>Next Todo</div>
+            <div>Next Todos</div>
             <nav>
                 <ul className='flex flex-row gap-5 justify-between'>
                     <li>
@@ -14,7 +14,7 @@ const MainNavigation: React.FC = () => {
                         <Link className='hover:text-blue-200' href="/todos" >All Todos</Link>
                     </li>
                     <li>
-                        <Link className='hover:text-blue-200' href="/new-todo">New Todo</Link>
+                        <Link className='hover:text-blue-200' href="/new-todo">New Todos</Link>
                     </li>
                 </ul>
             </nav>
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -24,11 +24,11 @@ const NewTodo: React.FC<{onSend : (text: string, description: string) => void}> 
     };
     return (
         <form onSubmit={submitHandler} className='self-center flex flex-col items-center justify-center w-9/10 text-blue-950'>
-            <label htmlFor="todo-text">Todo Text</label>
+            <label htmlFor="todo-text">Todos Text</label>
             <input  className='border-b-blue-950 border-2 rounded shadow pl-1'  type="text" id="todo-text" ref={textInputRef}/>
-            <label htmlFor="todo-description">Todo Description</label>
+            <label htmlFor="todo-description">Todos Description</label>
             <input className='border-b-blue-950 border-2 rounded shadow pl-1' type="text" id="todo-description" ref={descriptionInputRef}/>
-            <button disabled={disabled} type="submit" className='bg-blue-300 m-14 p-3 rounded shadow-blue-900 hover:bg-blue-200 disabled:opacity-50'>Add Todo</button>
+            <button disabled={disabled} type="submit" className='bg-blue-300 m-14 p-3 rounded shadow-blue-900 hover:bg-blue-200 disabled:opacity-50'>Add Todos</button>
         </form>
     )
 }
