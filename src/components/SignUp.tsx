31a4ebd096acf78c52965c7fbc1e2084a8d2aa6d
Hakan Urtimur <dythakanurtimur@gmail.com>
2023-06-08 14:27:52 +0300

    fix input bugs Signed-off-by: Hakan Urtimur <dythakanurtimur@gmail.com>
    Signed-off-by: Hakan Urtimur <dythakanurtimur@gmail.com>

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -34,9 +34,9 @@ const SignUp: React.FC = () => {
 
     return <form onSubmit={submitHandler} className='flex flex-col justify-between items-center text-blue-950 p-3 w-6/12 m-auto gap-2 border-2 border-b-blue-950'>
         <label htmlFor="email">Email</label>
-        <input className='bg-blue-300 m-14 p-3 rounded shadow-blue-900 hover:bg-blue-200 disabled:opacity-50' type="email" id="email" ref={emailRef}/>
+        <input className='bg-blue-300 m-1 p-3 rounded shadow-blue-900 hover:bg-blue-200 disabled:opacity-50' type="email" id="email" ref={emailRef}/>
         <label htmlFor="password">Password</label>
-        <input className='bg-blue-300 m-14 p-3 rounded shadow-blue-900 hover:bg-blue-200 disabled:opacity-50' type="password" id="password" ref={passwordRef}/>
+        <input className='bg-blue-300 m-1 p-3 rounded shadow-blue-900 hover:bg-blue-200 disabled:opacity-50' type="password" id="password" ref={passwordRef}/>
         <button type="submit" className='bg-blue-300 m-1 p-3 rounded shadow-blue-900 hover:bg-blue-200 disabled:opacity-50 border-2 border-b-blue-950'>Sign Up</button>
     </form>
 
