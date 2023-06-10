9cdb2288db147b42b123b6034f12210adbe93812
Hakan Urtimur <dythakanurtimur@gmail.com>
2023-06-10 04:19:07 +0300

    Fixing ui bugs  Hakan Urtimur <dythakanurtimur@gmail.com>
    Signed-off-by: Hakan Urtimur <dythakanurtimur@gmail.com>

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,45 +1,52 @@
-import React, {useContext} from "react";
-import {TodoContext} from "@/context/TodoContext";
+import React, { useContext } from "react";
+import { TodoContext } from "@/context/TodoContext";
 
+const TodoItem: React.FC<{
+  item: object;
+  title: string;
+  id: string;
+  isCompleted: boolean;
+  description: string;
+}> = (props) => {
+  const [isShown, setIsShown] = React.useState(false);
 
+  const { completeTodo, deleteTodo } = useContext(TodoContext);
 
+  const showDetailsHandler = () => {
+    setIsShown((prevState) => !prevState);
+  };
 
-const TodoItem: React.FC<{ item: object, title: string, id: string, isCompleted: boolean, description: string }> = (props) => {
+  const completeHandler = () => {
+    completeTodo(props.item);
+  };
 
+  const deleteHandler = () => {
+    deleteTodo(props.item);
+  };
 
-    const [isShown, setIsShown] = React.useState(false);
-
-    const {completeTodo, deleteTodo} = useContext(TodoContext);
-
-    const showDetailsHandler = () => {
-        setIsShown(prevState => !prevState);
-    }
-
-    const completeHandler = () => {
-        completeTodo(props.item)
-    }
-
-    const deleteHandler = () => {
-        deleteTodo(props.item)
-    }
-
-
-
-
-
-
-
-    return <li className='flex flex-col justify-between w-8/12 border-2 border-b-blue-950 text-blue-950 p-2 m-2 rounded'>
-        <div className='flex flex-row justify-between gap-1'>
-            <p className='text-blue-900 text-1xl whitespace-normal break-words w-60 '>{props.title}</p>
-            <div className='flex flex-row gap-1'>
-            <button onClick={completeHandler}>✔</button>
-            <button onClick={deleteHandler}>✘</button>
-            <button onClick={showDetailsHandler}>▼</button>
-            </div>
+  return (
+    <li className="flex flex-col justify-between w-8/12 border-2 border-b-blue-950 text-blue-950 p-2 m-2 rounded">
+      <div className="flex flex-row justify-between gap-1">
+        <p className="text-blue-900 text-1xl whitespace-normal break-words w-60 ">
+          {props.title}
+        </p>
+        <div className="flex flex-row gap-1">
+          <button className="text-green-400" onClick={completeHandler}>
+            ✔
+          </button>
+          <button className="text-red-600" onClick={deleteHandler}>
+            ✘
+          </button>
+          <button onClick={showDetailsHandler}>▼</button>
         </div>
-        {isShown && <p className='whitespace-normal break-words flex-grow-0 flex-shrink-0'>{props.description}</p>}
-        </li>
-        }
+      </div>
+      {isShown && (
+        <p className="whitespace-normal break-words flex-grow-0 flex-shrink-0">
+          {props.description}
+        </p>
+      )}
+    </li>
+  );
+};
 
 export default TodoItem;
--- src/components/TodoItemCompleted.tsx
+++ src/components/TodoItemCompleted.tsx
@@ -1,40 +1,45 @@
-import React, {useContext} from "react";
-import {TodoContext} from "@/context/TodoContext";
-
+import React, { useContext } from "react";
+import { TodoContext } from "@/context/TodoContext";
 
 const TodoItemCompleted: React.FC<{
-    title: string,
-    id: string,
-    isCompleted: boolean,
-    description: string
-    item: object
+  title: string;
+  id: string;
+  isCompleted: boolean;
+  description: string;
+  item: object;
 }> = (props) => {
+  const [isShown, setIsShown] = React.useState(false);
 
+  const { deleteTodo } = useContext(TodoContext);
 
-    const [isShown, setIsShown] = React.useState(false);
+  const deleteHandler = () => {
+    deleteTodo(props.item);
+  };
 
-    const {deleteTodo} = useContext(TodoContext);
+  const showDetailsHandler = () => {
+    setIsShown((prevState) => !prevState);
+  };
 
-    const deleteHandler = () => {
-        deleteTodo(props.item)
-    }
-
-    const showDetailsHandler = () => {
-        setIsShown(prevState => !prevState);
-    }
-
-
-    return <li
-        className='flex flex-shrink-0 flex-col justify-between w-8/12 border-2 border-b-blue-950 text-blue-950 p-3 m-3 rounded'>
-        <div className='flex  flex-row justify-between gap-1'>
-            <p className='text-blue-900 text-l whitespace-normal break-words w-60 '>{props.title}</p>
-            <div className='flex flex-row gap-1'>
-                <button onClick={deleteHandler}>✘</button>
-                <button onClick={showDetailsHandler}>▼</button>
-            </div>
+  return (
+    <li className="flex flex-shrink-0 flex-col justify-between w-8/12 border-2 border-b-blue-950 text-blue-950 p-3 m-3 rounded">
+      <div className="flex  flex-row justify-between gap-1">
+        <p className="text-blue-900 text-l whitespace-normal break-words w-60 ">
+          {props.title}
+        </p>
+        <div className="flex flex-row gap-1">
+          <button className="text-red-600" onClick={deleteHandler}>
+            ✘
+          </button>
+          <button onClick={showDetailsHandler}>▼</button>
         </div>
-        {isShown && <p className='whitespace-normal break-words flex-grow-0'>{props.description}</p>}
+      </div>
+      {isShown && (
+        <p className="whitespace-normal break-words flex-grow-0">
+          {props.description}
+        </p>
+      )}
     </li>
-}
+  );
+};
 
 export default TodoItemCompleted;
