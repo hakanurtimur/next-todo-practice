7c6bc95eccb35dc19a4376a927dfe07657697a16
Hakan Urtimur <dythakanurtimur@gmail.com>
2023-06-13 22:10:38 +0300

    Set todo class to an interface <dythakanurtimur@gmail.com>
    Signed-off-by: Hakan Urtimur <dythakanurtimur@gmail.com>

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -1,23 +1,9 @@
-class Todo {
+interface Todo {
   id: string;
   title: string;
   isCompleted: boolean;
   description: string;
   owner: string;
-
-  constructor(
-    title: string,
-    id: string,
-    isCompleted: boolean,
-    description: string,
-    owner: string
-  ) {
-    this.id = id;
-    this.title = title;
-    this.isCompleted = isCompleted;
-    this.description = description;
-    this.owner = owner;
-  }
 }
 
 export default Todo;
