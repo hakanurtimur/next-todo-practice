7bff4dc294a6a03c3d51d4cc954d898824be3dde
Hakan Urtimur <dythakanurtimur@gmail.com>
2023-09-25 00:03:10 +0300

    fix bugs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link';
+import Link from "next/link";
+import React from "react";
 
 const Home = () => {
   return (
@@ -7,7 +8,8 @@ const Home = () => {
         Welcome to Next Todo App
       </h1>
       <p className="text-lg mb-8 text-center">
-        Next Todo App is your ultimate task management solution. Stay organized and boost your productivity effortlessly.
+        Next Todo App is your ultimate task management solution. Stay organized
+        and boost your productivity effortlessly.
       </p>
       <div className="text-lg mb-8 text-left">
         <p className="mb-2">Key Features:</p>
@@ -15,17 +17,21 @@ const Home = () => {
           <li>Create and manage tasks seamlessly.</li>
           <li>Track your progress by marking tasks as completed.</li>
           <li>Effortlessly organize tasks by categories or due dates.</li>
-          <li>Access your to-do list from any device with an internet connection.</li>
+          <li>
+            Access your to-do list from any device with an internet connection.
+          </li>
         </ul>
       </div>
-      <Link href="/auth" className={" \"bg-yellow-400 hover:bg-yellow-500 text-gray-800 py-2 px-4 rounded-full text-lg font-semibold transition duration-300 ease-in-out\""}>
-          Get Started
+      <Link
+        href="/auth"
+        className={
+          ' "bg-yellow-400 hover:bg-yellow-500 text-gray-800 py-2 px-4 rounded-full text-lg font-semibold transition duration-300 ease-in-out"'
+        }
+      >
+        Get Started
       </Link>
       <p className="mt-4 text-lg">
-        Don't have an account?{' '}
-        <Link href="/sign-up">
-       Sign Up
-        </Link>
+        Do not have an account? <Link href="/sign-up">Sign Up</Link>
       </p>
     </div>
   );
